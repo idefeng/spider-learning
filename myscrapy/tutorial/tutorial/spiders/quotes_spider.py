960efd9ff279619c0ce8beb4e9c8da457938ecc0
zhangdf <define.zhang@qq.com>
2019-02-14 15:07:33 +0800

    添加链接url

--- myscrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ myscrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -19,7 +19,7 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            # 'http://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -31,3 +31,8 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            # next_page = response.urljoin(next_page)
+            # yield scrapy.Request(next_page, callback=self.parse)
+            yield response.follow(next_page, callback=self.parse)
