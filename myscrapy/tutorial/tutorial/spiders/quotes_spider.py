1647df5d62df719321f6066f9bdf97904769ddf1
zhangdf <define.zhang@qq.com>
2019-02-14 11:20:57 +0800

    修改爬虫爬取逻辑

--- myscrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ myscrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -13,7 +13,7 @@
 
 import scrapy
 
-class QuoteSpider(scrapy.Spider):
+class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
@@ -25,8 +25,9 @@ class QuoteSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
